b95685a8354a39ab6c2d6a1e414de94da3b60410
touhidul-islam-html <ti360109@gmail.com>
2024-11-17 17:45:48 +0600

    update

--- app/Components/HomeComponets/Hero.js
+++ app/Components/HomeComponets/Hero.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Button from '../Button'
 import Image from 'next/image'
+import canvapro from '@/public/images/canva-pro.png'
 export default function Hero() {
     return (
         <>
             <div className="video section text-center flex items-center justify-center py-10">
-                <Image src={"/public/images/Canva.png"} className='w-[40%] rounded-3xl shadow-lg' alt='hero' />
+                <Image src={canvapro} className='w-[40%] rounded-3xl shadow-lg' alt='hero' />
             </div>
             <Button link={"https://www.facebook.com/brittodigitalbd"} text={"Order Now"} />
         </>
