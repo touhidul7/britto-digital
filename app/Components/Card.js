3024b9f96b138e5ae2eb6904dd6aad11a946b286
touhidul-islam-html <ti360109@gmail.com>
2024-11-17 10:35:43 +0600

    update

--- app/Components/Card.js
+++ app/Components/Card.js
@@ -1,46 +1,43 @@
+'use client'
 import Image from "next/image";
 import React from "react";
-export default function Card({img, heading, text, bg}) {
+export default function Card({ img, heading, text, bg, imgsize }) {
+  const cardStyle = {
+    backgroundImage: `
+    linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), 
+    url('https://demo.brittosoft.com/wp-content/uploads/2024/11/Add-a-heading-4-1.webp')
+  `,
+    backgroundPosition: "center center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+  };  
   return (
-     <>
-     {bg==='white' ? (
-        <div className="flex flex-col gap-4 bg-white text-black">
+    <>
+      {bg === "white" ? (
+        <div className="flex flex-col gap-4 bg-white text-black p-6 rounded-2xl box-border">
           <Image
-            className="w-[13%]"
+            className={`${imgsize==="small" ? "w-[8%]":"w-[13%]"}`}
             src={img}
             width={100}
             height={100}
             alt="image"
           />
-          <div className="heading text-[32px] font-extrabold">
-            {heading}
-          </div>
-          <div className="text">
-            {text}
-          </div>
+          <div className="heading text-[32px] font-extrabold">{heading}</div>
+          <div className="text">{text}</div>
         </div>
       ) : (
-        <div className="flex flex-col gap-4 bg-black text-white">
+        <div className="flex flex-col gap-4  text-white p-6 rounded-2xl box-border" style={cardStyle}>
           <Image
-            className="w-[13%]"
-            src={cardimg}
+            className={`${imgsize==="small" ? "w-[8%]":"w-[13%]"}`}
+            src={img}
             width={100}
             height={100}
             alt="image"
           />
-          <div className="heading text-[32px] font-extrabold">
-            ফিউচার ইজ লীডস c
-          </div>
-          <div className="text">
-            ফিউচার ইজ লিড জেনারেশন এবং আমরা এখন বসে আছি সেই ভবিষ্যতের
-            দ্বারপ্রান্তে। যদি আপনি এই পরিবর্তনকে এক্সেপ্ট করে এগিয়ে যেতে পারেন
-            এবং অন্যদের আগে নিজেকে এই জার্নিতে শামিল করে নিতে পারেন, তাহলে আপনি
-            সফলতা অন্যদের থেকে বহুগুণ আগে পেয়ে যাবেন। তবে ডিসিশনটা আপনার, আপনি
-            পিছিয়ে থাকবেন নাকি এগিয়ে যাবেন।
-          </div>
+          <div className="heading text-[32px] font-extrabold">{heading}</div>
+          <div className="text">{text}</div>
         </div>
       )}
-     </>
-    
+    </>
   );
 }
